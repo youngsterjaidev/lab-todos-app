ff530c4125f6634bdfad08cd44ddf3341237a5eb
youngsterjaidev <jaidevv999@gmail.com>
2024-08-26 13:42:23 +0530

    adding the todo routes

--- app.py
+++ app.py
@@ -1,7 +1,40 @@
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS
+import mysql.connector
 
 app = Flask(__name__)
+CORS(app)
 
+# connecting the sql server
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="magicbus",
+  password="password",
+  database="todoApp"
+)
+
+# fetch all the todos from the database
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+		mycursor = mydb.cursor()
+
+		mycursor.execute("SELECT * FROM todos")
+
+		myresult = mycursor.fetchall()
+
+		print(myresult)
+		return myresult
+
+@app.route("/add", methods=["post"])
+def add_todo():
+		let req_data = request.json
+		mycursor = mydb.cursor()
+		# store the todo in database
+		mycursor.execute("INSERT INTO todos (title, timestamp, isCompleted ) VALUES ('Watch some series', '2024-08-26 13:02:01', false);")
+
+		mydb.commit()
+
+		print(mycursor.rowcount, "record inserted.")
+
+		# fetch todo from request
+		return "Todo added!"
