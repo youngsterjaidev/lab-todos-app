49d87d4da0acdc65fb051a9fcbea9e9b57537ef0
youngsterjaidev <jaidevv999@gmail.com>
2024-08-26 13:40:28 +0530

    adding the todo support

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,64 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import viteLogo from '/vite.svg';
 
 function App() {
   const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([])
+
+  const fetchTodos = ()=> {
+    // make a get resquest server
+    // find the data from response
+    axios.get("http://192.168.0.253:5000/")
+      .then(res => {
+        console.log(res.data)
+        setTodos(res.data)
+      })
+  }
+  
+  const handleSubmit = (e) => {
+    // prevent page from reloading
+    e.preventDefault()
+
+    // send the post request to the server
+    axios.post("http://192.168.0.253:5000/add", {
+      title: "football",
+      isCompleted: false,
+      timestamp: "2024-08-26 13:35:04"
+    }).then(res => {
+      console.log(
+        "Response ", res
+      )
+
+      fetchTodos()
+    })
+  }
+
+  useEffect(() => {
+    fetchTodos()
+  }, [])
 
   return (
-    <>
+    <div>
+    <form onSubmit={handleSubmit}>
+      <div><input type="text" placeholder='Enter the todo' /></div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button type='submit'>Add Todo</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </form>
+    <section>
+      <ul>
+        {todos.map((todo) => {
+          return (
+          <li>
+            <input type='checkbox' checked={todo[3]} />
+            {todo[1]}
+          </li>)
+        })}
+      </ul>
+    </section>
+    </div>
   )
 }
 
