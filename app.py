4992e0c3620d7929a7269507703fa3ab2d7eca8e
youngsterjaidev <jaidevv999@gmail.com>
2024-08-27 11:55:01 +0530

    updating the db credentails

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 from flask import Flask, request
 from flask_cors import CORS
 import mysql.connector
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
 
 app = Flask(__name__)
 CORS(app)
 
 # connecting the sql server
 mydb = mysql.connector.connect(
-  host="localhost",
-  user="magicbus",
-  password="password",
-  database="todoApp"
+  host=config["HOSTNAME"],
+  user=config["USER"],
+  password=config["PASS"],
+  database=config["DB"]
 )
 
 # fetch all the todos from the database
@@ -27,7 +30,7 @@ def hello_world():
 
 @app.route("/add", methods=["post"])
 def add_todo():
-		let req_data = request.json
+		req_data = request.json
 		mycursor = mydb.cursor()
 		# store the todo in database
 		mycursor.execute("INSERT INTO todos (title, timestamp, isCompleted ) VALUES ('Watch some series', '2024-08-26 13:02:01', false);")
